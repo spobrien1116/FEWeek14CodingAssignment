318eba10fb92f393c06a0f9d28bf23549e491044
spobrien1116 <87784429+spobrien1116@users.noreply.github.com>
2022-08-30 18:54:12 -0400

    Started movie-app framework
    Just wanted to begin fleshing out the framework for the project, but only  managed to work on Movie.js. This will be a start for my work tomorrow.

---
+++ movie-app/src/components/Movie.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class Movie extends React.Component {
+    render() {
+        return (
+            <div>
+                <img src='#'></img>
+                <p>Synopsis</p>
+                <div>Rating</div>
+                <br></br>
+                <div>Reviews
+
+                </div>
+                <div>Review Form
+                    <form>
+                        
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
